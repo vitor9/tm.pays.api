1d4d0ea46f4f15059c18d2ca992f529e7fc3b00f
vitor9 <vtorzy@gmail.com>
2023-11-29 14:23:35 -0300

    Atualizado código para poder realizar os testes unitários com o lombok

---
+++ src/test/java/com/tm/payments/api/TransferenciaRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.tm.payments.api;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.tm.payments.api.model.Transferencia;
+import com.tm.payments.api.repository.TransferenciaRepository;
+
+@DataJpaTest
+public class TransferenciaRepositoryTest {
+
+	@Autowired
+    private TransferenciaRepository transferenciaRepository;
+
+    @Test
+    @Transactional
+    public void testSalvarEConsultarTransferencia() {
+        // Criação de uma instância de Transferencia
+        Transferencia transferencia = new Transferencia();
+        transferencia.setContaOrigem("123456");
+        transferencia.setContaDestino("789012");
+        transferencia.setValorTransferencia(BigDecimal.valueOf(100.00));
+        transferencia.setDataTransferencia(LocalDate.now());
+
+        // Salva a transferência no banco de dados
+        transferenciaRepository.save(transferencia);
+
+        // Consulta a transferência do banco de dados
+        Transferencia transferenciaSalva = transferenciaRepository.findById(transferencia.getId()).orElse(null);
+        transferenciaRepository.findAll();
+
+        // Verifica se a transferência foi salva e consultada corretamente
+        assertThat(transferenciaSalva).isNotNull();
+        assertThat(transferenciaSalva.getContaOrigem()).isEqualTo("123456");
+        assertThat(transferenciaSalva.getContaDestino()).isEqualTo("789012");
+        assertThat(transferenciaSalva.getValorTransferencia()).isEqualTo(BigDecimal.valueOf(100.00));
+        assertThat(transferenciaSalva.getDataTransferencia()).isEqualTo(LocalDate.now());
+        
+    }
+	
+}
